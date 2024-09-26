84facfbec7fe3854647b5b25967021f516d44bb9
CristianMartinezD <cristian.martinenez@udea.edu.co>
2024-09-26 12:21:52 -0500

    Termine todo el codigo que codifica y decodifica, y empece la aplicacion

--- Laborarorio_3/main.cpp
+++ Laborarorio_3/main.cpp
@@ -10,12 +10,14 @@ using namespace std;
 string convertirTEXTO_A_binario(string);
 void escribirArchivo(string, string, bool);
 string invertir_bloque(const string&);
-string procesar_binario(const string&, int, string); //Recibe el binario que voy a procesar, n, y la acción que voy a realizar, ya sea codificar o decodificar.
-string decodificar_contenido(const string&, int);
+string metodo1(const string&, int, string); //Recibe el binario que voy a procesar, n, y la acción que voy a realizar, ya sea codificar o decodificar.
+string metodo2(const string&, int, string);
+void EscribirArchivoBinario(const string&, const string&);
 //string leerArchivoPorCaracter(string);
 string leerArchivoPorLinea(string);
 int obtenerENTEROpositivo();
 int obtenerENTERO();
+void menuOperacionesBancarias(); void registrarUsuario(); void consultarSaldo(); void retirarDinero();
 
 
 int main()
@@ -24,8 +26,8 @@ int main()
 
     while (salir == false) {
         cout<<"\nMENU PRINCIPAL";
-        cout<<"\nQue operacion deseas realizar?\n1. Codificar\n2. Decodificar\n0. Salir\nELIGE UNA OPCION (1, 2, o 0): ";cin>>opcion;
-        if (opcion != '0') {cout<<"\nCOMO SE LLAMA EL ARCHIVO FUENTE: "; cin >> nombreArchivoFuente;}
+        cout<<"\nQue operacion deseas realizar?\n1. Codificar\n2. Decodificar\n3. Operaciones bancarias\n0. Salir\nELIGE UNA OPCION (1, 2, 3 o 0): ";cin>>opcion;
+        if (opcion == '1' || opcion == '2') {cout<<"\nCOMO SE LLAMA EL ARCHIVO FUENTE: "; cin >> nombreArchivoFuente;}
 
         switch (opcion) {
 
@@ -43,19 +45,31 @@ int main()
                     cout<<"\nINGRESA UN VALOR VALIDO, 1 o 2: "; metodo_codificacion = obtenerENTEROpositivo();
                 }
 
-                cout << "\nContenido del archivo original en BINARIO:\n\n" << binario << endl;
+                cout <<"\nQue nombre le quieres poner al archivo de salida: "; cin >>nombreArchivoSalida;
+                cout << "\n\nContenido del archivo original en BINARIO:\n\n" << binario << endl;
 
                 /* AQUÍ EMPEZAMOS CON LA CODIFICACIÓN */
                 if (metodo_codificacion == 1){
-                string contenido_codificado = procesar_binario(binario, n, "codificar");
-                cout << "\nContenido del archivo codificado:\n\n" << contenido_codificado << endl;
+                string binario_codificado = metodo1(binario, n, "codificar");
+                cout << "\nEl binario codificado es:\n\n" << binario_codificado << endl;
+
+                EscribirArchivoBinario(nombreArchivoSalida, binario_codificado);
+                cout << "\nEL TEXTO CODIFICADO SE GUARDO EN UN ARCHIVO LLAMADO: " << nombreArchivoSalida << endl <<endl;
+
                 }
-                else{/*AQUI LLAMO LA FUNCION DEL METODO DOS */}
+                else{
+                    string binario_codificado = metodo2(binario, n, "codificar");
+                    cout << "\nEl binario codificado es:\n\n" << binario_codificado << endl;
+
+                    EscribirArchivoBinario(nombreArchivoSalida, binario_codificado);
+                    cout << "\nEL TEXTO CODIFICADO SE GUARDO EN UN ARCHIVO LLAMADO: " << nombreArchivoSalida << endl <<endl;
+
+                }
                 break;
             }
 
             case '2':{ /* DECODIFICAR */
-                string binario_codificado = leerArchivoPorLinea(nombreArchivoFuente);
+                string binario_codificado = convertirTEXTO_A_binario(nombreArchivoFuente);
                 if(binario_codificado == "") {cout<<"\nArchivo NO encontrado\n";return 1;}
 
                 cout<<"\nCual es la semilla n para la decodificacion: "; n = obtenerENTEROpositivo();
@@ -67,13 +81,25 @@ int main()
                     cout<<"\nINGRESA UN VALOR VALIDO, 1 o 2: "; metodo_codificacion = obtenerENTEROpositivo();
                 }
 
+                cout <<"\nQue nombre le quieres poner al archivo de texto de salida: "; cin >>nombreArchivoSalida;
                 if (metodo_codificacion == 1){
-                    string binario_decodificado = procesar_binario(binario_codificado, n, "decodificar");
-                    cout << "\nBinario decodificado:\n\n" << binario_decodificado << endl <<endl;
+                    string binario_decodificado = metodo1(binario_codificado, n, "decodificar");
+                    cout << "\nEl binario decodificado es:\n\n" << binario_decodificado << endl;
+
+                    EscribirArchivoBinario(nombreArchivoSalida, binario_decodificado);
+                    cout << "\nEL TEXTO DECODIFICADO SE GUARDO EN UN ARCHIVO LLAMADO: " << nombreArchivoSalida << endl <<endl;
                 }
-                else {/*AQUI LLAMO LA FUNCION DEL METODO DOS */}
+                else {
+                    string binario_decodificado = metodo2(binario_codificado, n, "decodificar");
+                    cout << "\nEl binario decodificado es:\n\n" << binario_decodificado << endl;
+
+                    EscribirArchivoBinario(nombreArchivoSalida, binario_decodificado);
+                    cout << "\nEL TEXTO DECODIFICADO SE GUARDO EN UN ARCHIVO LLAMADO: " << nombreArchivoSalida << endl <<endl;
+                }
                 break;
             }
+
+            case '3': menuOperacionesBancarias(); break;
 
             default: {cout<<"\nNO INGRESASTE UNA OPCION VALIDA\n"; break;}
         }
@@ -100,10 +126,8 @@ string convertirTEXTO_A_binario(string nombreArchivo){
     string str_binario;
 
     while (archivo.get(caracter)) {
-        if (caracter != '\n'){
-            bitset<8> binario(caracter);
-            str_binario += binario.to_string();
-        }
+        bitset<8> binario(caracter);
+        str_binario += binario.to_string();
     }
 
     archivo.close();
@@ -112,7 +136,7 @@ string convertirTEXTO_A_binario(string nombreArchivo){
 
 /* FUNCIÓN PARA CODIFICAR Y DECODIFICAR EL BINARIO */
 
-string procesar_binario(const string& binarioAprocesar, int n, string accionArealizar) {
+string metodo1(const string& binarioAprocesar, int n, string accionArealizar) {
     // Dividimos el binario en bloques de tamaño n
     vector<string> bloques;
     for (size_t i = 0; i < binarioAprocesar.size(); i += n) {
@@ -182,6 +206,61 @@ string invertir_bloque(const string& bloque) {
 
 
 
+/* FUNCION PARA CODIFICAR Y DECODIFICAR MEDIANTE EL METODO 2 */
+string metodo2(const string& binarioAprocesar, int n, string accionArealizar) {
+    // Dividimos el binario en bloques de tamaño n
+    vector<string> bloques;
+    for (size_t i = 0; i < binarioAprocesar.size(); i += n) {
+        bloques.push_back(binarioAprocesar.substr(i, n));
+    }
+
+    // Para almacenar el resultado final
+    string resultado;
+
+    if (accionArealizar == "codificar") {
+        // Codificamos cada bloque
+        for (size_t i = 0; i < bloques.size(); ++i) {
+            string bloqueActual = bloques[i];
+            // Desplazamos cada bit hacia la derecha
+            char ultimoBit = bloqueActual[bloqueActual.length() - 1];
+            for (size_t j = bloqueActual.length() - 1; j > 0; --j) {
+                bloqueActual[j] = bloqueActual[j - 1];
+            }
+            bloqueActual[0] = ultimoBit;
+            resultado += bloqueActual;
+        }
+    }
+    else if (accionArealizar == "decodificar") {
+        // Decodificamos cada bloque
+        for (size_t i = 0; i < bloques.size(); ++i) {
+            string bloqueActual = bloques[i];
+            // Desplazamos cada bit hacia la izquierda
+            char primerBit = bloqueActual[0];
+            for (size_t j = 0; j < bloqueActual.length() - 1; ++j) {
+                bloqueActual[j] = bloqueActual[j + 1];
+            }
+            bloqueActual[bloqueActual.length() - 1] = primerBit;
+            resultado += bloqueActual;
+        }
+    }
+    else return ""; // Para el caso donde no se envió una acción válida.
+
+    return resultado;
+}
+
+
+
+// Función para escribir la cadena de bits codificados en un archivo binario
+void EscribirArchivoBinario(const string& nombreArchivo, const string& binarioCodificado) {
+
+    ofstream archivo(nombreArchivo, ios::binary);
+    for (size_t i = 0; i < binarioCodificado.size(); i += 8) {
+        bitset<8> byte(binarioCodificado.substr(i, 8));
+        archivo.put(static_cast<unsigned char>(byte.to_ulong()));
+
+    }
+}
+
 
 
 void escribirArchivo(string nombreArchivo, string texto, bool limpiar=false)
@@ -249,6 +328,42 @@ string leerArchivoPorCaracter(string nombreArchivo)
 
     return contenido;
 }
+
+
+
+
+
+/* APLICACION */
+
+void menuOperacionesBancarias(){
+    system("cls"); cout <<"\n   BIENVENIDO AL BANCO.\n"; system("pause"); system("cls");
+
+    bool salir = false;
+    while (salir == false) {
+        cout <<"\nMENU DE OPERACIONES BANCARIAS\n";
+        char operacion;
+        cout <<"\nQue operacion deseas realizar:\n";
+        cout <<"1. Registrar usuarios en el sistema (solo admin)\n";
+        cout <<"2. Consultar saldo\n";
+        cout <<"3. Retirar dinero\n";
+        cout <<"0. Volver al menu principal\n";
+        cout <<"INGRESA UNA DE LAS ANTERIORES OPCIONES (1,2,3, o 0): "; cin >> operacion;
+
+        switch (operacion) {
+        case '1': registrarUsuario(); break;
+        case '2': consultarSaldo(); break;
+        case '3': retirarDinero(); break;
+        case '0': salir = true; break;
+        default:break;
+        }
+    }
+}
+
+
+
+void registrarUsuario(){}
+void consultarSaldo(){}
+void retirarDinero(){}
 
 
 
